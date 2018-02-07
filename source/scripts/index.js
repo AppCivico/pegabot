d2094e27b17bac6af49063f53ee6fdc9b9048d35
Robson Sobral <robson@oficiodesign.com>
2018-02-06 23:50:09 -0200

    Improve VueJS code

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,73 +1,69 @@
 /* global Vue */
 
-function query(uri) {
-	const queryString = uri || window.location.search;
-	const params = {};
-	const queries = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
-
-	for (let i = 0; i < queries.length; i += 1) {
-		const element = queries[i].split('=');
-		params[decodeURIComponent(element[0])] = decodeURIComponent(element[1] || '');
-	}
-
-	return params;
-}
-
-const data = {
-	profileList: [
-		{
-			username: 'Laís',
-			url: '.',
-			avatar: 'valiria.com/avatar',
-			content: {
-				value: 0.5,
-			},
-			language_independant: {
-				friend: 0.5,
-				temporal: 0.5,
-				network: 0.5,
-				user: 0.5,
-			},
-			language_dependent: {
-				content: 0.5,
-				sentiment: 0.5,
-			},
-			bot_probability: {
-				all_features: 0.5,
-				language_independent: 0.5,
-			},
-			share_link_on_social_network: '.',
-			user_profile_language: '.',
-			feedback_report_link: '.',
-		},
-	],
-};
-
-
-data.current = 0;
-data.downloadResults = {
-	value: '.',
-};
-
-data.query = query();
-
-Vue.component('profile', {
-	template: '#profile',
-	props: [
-		'user',
-	],
-});
-
-const app = new Vue({
+window.$vue = new Vue({
 	el: '#results__list',
-	data,
+	data: {
+		debug: true,
+		profileList: [],
+		query: {},
+		loading: false,
+		error: null,
+		profilesMeta: {
+			current: 0,
+			total: 0,
+			downloadResults: {
+				value: '.',
+			},
+		},
+	},
+	components: {
+		profile: {
+			template: '#profile',
+			props: [
+				'user',
+			],
+		},
+	},
 	methods: {
+		getQueryString(uri) {
+			const queryString = uri || window.location.search;
+			const params = {};
+			const queries = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+
+			for (let i = 0; i < queries.length; i += 1) {
+				const element = queries[i].split('=');
+				params[decodeURIComponent(element[0])] = decodeURIComponent(element[1] || '');
+			}
+
+			return params;
+		},
+		loadProfiles() {
+			this.error = null;
+			this.loading = true;
+
+			this.$http.get('/assets/data/test.json', { params: this.query })
+				.then((response) => {
+					this.loading = false;
+					if (response.status === 200) {
+						this.profileList = response.body.profiles;
+						this.profilesMeta.total = response.body.profiles.length;
+					}
+				}, (error) => {
+					alert(error.statusText); // eslint-disable-line no-alert
+				});
+		},
+		showElement() {
+			if (this.$el.hasAttribute('hidden')) {
+				this.$el.removeAttribute('hidden');
+			}
+		},
 	},
 	created() {
+		this.query = this.getQueryString();
 	},
 	mounted() {
-		if (this.$el.hasAttribute('hidden')) {
-			this.$el.removeAttribute('hidden');
-		}
+		this.loadProfiles();
+		this.showElement();
 	},
 });
+
