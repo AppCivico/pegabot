c0c0df38486f4afdbd2b75aecae486cb67607fc0
Robson Sobral <robson@oficiodesign.com>
2020-10-13 11:47:57 -0300

    Normalize chart color parameter

--- assets/scripts/TheChart.js
+++ assets/scripts/TheChart.js
@@ -17,21 +17,28 @@ export default {
 
 	mounted() {
 		const { labels, datasets = [], data } = this.data;
+		let normalizedDatasets;
 
 		if (!datasets.length) {
 			if (data) {
-				datasets.push({
+				normalizedDatasets = datasets.concat([{
 					data,
 					label: 'Tweets',
 					backgroundColor: 'rgb(26, 145, 208)',
 					borderWidth: 0, // <-- set this for a plain legend
-				});
+				}]);
 			} else {
 				throw new Error('unexpected data layout');
 			}
+		} else {
+			normalizedDatasets = Object.keys(datasets)
+				.map(x => (!datasets[x].backgroundColor && !!datasets[x].color
+					? Object.assign(datasets[x], { backgroundColor: datasets[x].color })
+					: datasets[x]));
 		}
 
-		this.renderChart({ labels, datasets }, options);
+
+		this.renderChart({ labels, datasets: normalizedDatasets }, options);
 	},
 
 	beforeDestroy() {
